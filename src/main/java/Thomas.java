e3bd3412a8b8348aa5414ad9325c614b3bc05986
Thomas Chiu <e1496731@u.nus.edu>
2025-02-07 13:47:57 +0800

    Enable ability to echo user's input, exit when it is bye

--- src/main/java/Thomas.java
+++ src/main/java/Thomas.java
@@ -1,9 +1,20 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Thomas {
     public static void main(String[] args) {
         String greet = "Hello! I'm Thomas\n" +
-                "What can I do for you?\n" +
-                "Bye. Hope to see you again soon!";
+                "What can I do for you?\n";
+        String bye = "Bye. Hope to see you again soon!";
 
+        Scanner command = new Scanner(System.in);
         System.out.println(greet);
+        String input = command.nextLine();
+        while (!Objects.equals(input, "bye")) {
+            System.out.println(input);
+            input = command.nextLine();
+        }
+
+        System.out.println(bye);
     }
 }
