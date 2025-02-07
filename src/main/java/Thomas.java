0144f1ca77a6639e3be3b794dc82f3bb6060651d
Thomas Chiu <e1496731@u.nus.edu>
2025-02-07 12:52:44 +0800

    Reapply "Level-0"
    This reverts commit 1f4bfd9d9b23ffc7f9ab2a28ebabd552d2c029f2.

---
+++ src/main/java/Thomas.java
@@ -0,0 +1,9 @@
+public class Thomas {
+    public static void main(String[] args) {
+        String greet = "Hello! I'm Thomas\n" +
+                "What can I do for you?\n" +
+                "Bye. Hope to see you again soon!";
+
+        System.out.println(greet);
+    }
+}
